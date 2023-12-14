45aadada49eb89ef1ba55acaba37372b19b7fff6
Ozumah Ebenezer <ozumahe@gmail.com>
2023-12-14 14:40:54 +0100

    Application SetUp

---
+++ src/components/Nav.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/nav.scss";
+
+function Nav() {
+  return (
+    <div className="nav-container">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, quam?
+      Aliquid vel illo accusamus, quam sed doloremque tempore dolore, velit
+      laborum quod fugit dolorum culpa, enim consequatur quidem quos pariatur!
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/components/Table.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/table.scss";
+
+type Props = {};
+
+function Table({}: Props) {
+  return <div>index</div>;
+}
+
+export default Table;
