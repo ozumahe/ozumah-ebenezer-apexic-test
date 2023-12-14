af2a34683025d0340c20e45d9f92d3b148cbe07d
Ozumah Ebenezer <ozumahe@gmail.com>
2023-12-14 15:55:36 +0100

    NavBar

---
+++ src/components/SVGS/index.ts
@@ -0,0 +1,6 @@
+export { default as Logo } from "./Logo";
+export { default as Hamburger } from "./Hamburger";
+export { default as SearchIcon } from "./SearchIcon";
+export { default as SettingsIcon } from "./SettingsIcon";
+export { default as NotificationIcon } from "./NotificationIcon";
+export { default as UserIcon } from "./UserIcon";
